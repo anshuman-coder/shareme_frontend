0bc1ef95744123d3125ed505df6df88862b5406b
anshuman-coder <65159507+anshuman-coder@users.noreply.github.com>
2022-02-24 03:18:07 +0530

    home_page_ongoing

--- src/Container/Home.jsx
+++ src/Container/Home.jsx
@@ -3,6 +3,9 @@ import { HiMenu } from 'react-icons/hi';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { Link, Route, Routes } from 'react-router-dom';
 
+// importing the Utils
+import { userQuery } from '../Utils/data';
+
 
 // importing the components files
 import { Sidebar, UserProfile } from '../Components';
@@ -16,9 +19,43 @@ import logo from '../assets/logo.png';
 
 
 const Home = () => {
+
+  const [toggleSidebar, setToggleSidebar] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+  useEffect(() => { 
+    const query = userQuery(userInfo?.googleId);
+
+    client.fetch(query)
+      .then((data) => {
+        setUser(data[0]);
+      });
+  }, []);
+
   return (
-    <div>
-      Home
+    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
+      <div className='hidden md:flex h-screen flex-initial' >
+        <Sidebar />
+      </div>
+      <div className='flex md:hidden flex-row'>
+        <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)} />
+        <Link to='/'>
+          <img src={logo} alt='logo' className='w-28'/>
+        </Link>
+        <Link to={`user-profile/${user?._id}`} >
+          <img src={user?.image} alt='logo' className='w-28'/>
+        </Link>
+      </div>
+      {toggleSidebar && (
+        <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in' >
+          <div className='absolute w-full flex justify-end items-center p-2' >
+            <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+          </div>
+          <Sidebar />
+        </div>
+      ) }
     </div>
   )
 }
